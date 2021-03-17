48028bb8d2ae019cdec72e26a01185a2c76791e6
Shlomi <vgtvgy1@gmail.com>
2021-03-18 00:53:52 +0200

    more stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-const { app, BrowserWindow, BrowserView } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-exports.printMsg = function () {
-	console.log("This is a message from the demo package");
-}
-
-//require('electron-reload')(__dirname, { electron: path.join(__dirname, 'node_modules', 'electron', 'dist', 'electron.exe') });
-
-
-const createWindow = () => {
+const appReady = () => {
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
 		webPreferences: {
@@ -30,7 +23,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', appReady);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -45,7 +38,7 @@ app.on('activate', () => {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
+		appReady();
 	}
 });
 
--- src/renderer.js
+++ src/renderer.js
@@ -2,14 +2,6 @@ const ElectronTabs = require("./electron-browser")
 const electronTabs = new ElectronTabs()
 
 electronTabs.addTab("Test", "test.html")
-electronTabs.addTab("Test aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "test.html")
-electronTabs.addTab("Test", "test.html")
-electronTabs.addTab("Test", "test.html")
-electronTabs.addTab("Test aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "test.html")
-
-electronTabs.addTab("Test aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "test.html")
-
-electronTabs.addTab("Test aaaaaaaaaaaaaaaaaaaaaaaaaaaa", "test.html")
-
-// electronTabs.addTab("Google", "https://google.com", "favicons/google-favicon.ico")
-// electronTabs.addTab("YouTube", "https://youtube.com", "favicons/youtube-favicon.ico")
+electronTabs.addTab("Google", "https://google.com", "favicons/google-favicon.ico")
+electronTabs.addTab("Facebook", "https://facebook.com", "favicons/facebook-favicon.ico")
+electronTabs.addTab("YouTube", "https://youtube.com", "favicons/youtube-favicon.ico")
