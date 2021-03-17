1af0a12672eafe50cd2698e08ea1709c61826fa3
Shlomi <vgtvgy1@gmail.com>
2021-03-18 01:49:44 +0200

    good changes

--- src/renderer.js
+++ src/renderer.js
@@ -5,3 +5,8 @@ electronTabs.addTab("Test", "test.html")
 electronTabs.addTab("Google", "https://google.com", "favicons/google-favicon.ico")
 electronTabs.addTab("Facebook", "https://facebook.com", "favicons/facebook-favicon.ico")
 electronTabs.addTab("YouTube", "https://youtube.com", "favicons/youtube-favicon.ico")
+
+document.querySelector('.btn-toggle').addEventListener('click', function () {
+	// Then toggle (add/remove) the .dark-theme class to the body
+	document.body.classList.toggle('dark-theme');
+})
