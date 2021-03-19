22130fb140d147e98ea69b32a8536a22daa2ccf5
Shlomi <vgtvgy1@gmail.com>
2021-03-19 14:43:24 +0200

    ready-for-production

--- src/example/renderer.js
+++ src/example/renderer.js
@@ -6,7 +6,7 @@ electronTabs.addTab("Google", "https://google.com", "favicons/google-favicon.ico
 electronTabs.addTab("Facebook", "https://facebook.com", "favicons/facebook-favicon.ico")
 electronTabs.addTab("YouTube", "https://youtube.com", "favicons/youtube-favicon.ico")
 
-document.querySelector('.btn-toggle').addEventListener('click', function () {
+document.querySelector('.btn-toggle-theme').addEventListener('click', function () {
 	// Then toggle (add/remove) the .dark-theme class to the body
 	document.body.classList.toggle('dark-theme');
 })
